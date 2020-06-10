0d2bafa86f2a5128c377a2b70c29ee30c55bc6b3
marcosaurius <marcos.e.martinez.97@gmail.com>
2020-06-09 22:13:13 -0500

    aun afnd1

--- AFND/afnd1.py
+++ AFND/afnd1.py
@@ -8,15 +8,33 @@ def q0(cadena, indice):
 
 
 def q1(cadena, indice):
-    if cadena[indice+1] != "\0":
-        if(cadena[indice+1] == "b" or cadena[indice] == "a"):
-            print("Cadena invalida")
-        elif cadena[indice+1] == "\0":
-            print("Cadena válida")
+    if indice < tam-1:
+        print("Cadena Invalida")
+
+    elif indice == tam-1:
+        print("Cadena válida")
+
+
+def q2(cadena,indice):
+    if indice == tam-1:
+        print("Cadena válida")
+    elif indice < tam-1:
+        if cadena[indice+1] == "b":
+            indice = indice + 1
+            q3(cadena, indice)
+        elif cadena[indice+1] == "a":
+            indice = indice + 1
+            q4(cadena, indice)
+        else:
+            print("Cadena Invalida")
+    #if indice+1 < tam-1:
+
+
 
 
 
 cadena = input("ingresa la cadena que quieras validar con la expresion regular: a(a*b|b*) | b: \n")
-print(cadena)
+tam = len(cadena)
+
 
 q0(cadena, 0)
