6325c4934eb03ba200f65b2dce770b0447927f8a
marcosaurius <marcos.e.martinez.97@gmail.com>
2020-06-10 19:33:35 -0500

    primer automata

--- AFND/afnd1.py
+++ AFND/afnd1.py
@@ -1,8 +1,8 @@
 def q0(cadena, indice):
     if cadena[indice] == "a":
-        q2(cracter[indice], indice)
+        q2(cadena, indice)
     elif cadena[indice] == "b":
-        q1(cadena[indice], indice)
+        q1(cadena, indice)
     else:
         print("Cadena invalida")
 
@@ -16,17 +16,56 @@ def q1(cadena, indice):
 
 
 def q2(cadena,indice):
+    #print(str(indice+1))
     if indice == tam-1:
         print("Cadena válida")
+    elif indice < tam-1:
+        indice = indice +1
+        print(cadena[indice] + " " + str(indice))
+        if cadena[indice] == "b":
+            #indice = indice + 1
+            q3(cadena, indice)
+        elif cadena[indice] == "a":
+            #indice = indice + 1
+            q4(cadena, indice)
+        else:
+            print("Cadena Invalida")
+
+
+def q3 (cadena, indice):
+    if indice == tam-1:
+        print("Cadena Válida")
     elif indice < tam-1:
         if cadena[indice+1] == "b":
             indice = indice + 1
             q3(cadena, indice)
-        elif cadena[indice+1] == "a":
-            indice = indice + 1
-            q4(cadena, indice)
         else:
-            print("Cadena Invalida")
+            print("cadena invalida")
+
+
+def q4(cadena, indice):
+    if indice == tam-1:
+        print("Cadena Invalida")
+    elif indice < tam -1:
+        if cadena[indice+1] == "a":
+            indice = indice +1
+            q4(cadena, indice)
+        elif cadena[indice +1] == "b":
+            indice = indice +1
+            q5(cadena, indice)
+        else:
+            print("Cadena invalida")
+
+
+def q5(cadena, indice):
+    if indice == tam-1:
+        print("Cadena valida")
+    elif indice < tam-1:
+        if cadena[indice+1] == "b" or cadena [indice+1] == "a":
+            print("Cadena invalida")
+        else:
+            print ("Cadena invalida")
+
     #if indice+1 < tam-1:
 
 
@@ -34,7 +73,9 @@ def q2(cadena,indice):
 
 
 cadena = input("ingresa la cadena que quieras validar con la expresion regular: a(a*b|b*) | b: \n")
+
+#print(cadena[1])
 tam = len(cadena)
-
-
-q0(cadena, 0)
+#print(str(tam))
+ind = 0
+q0(cadena, ind)
